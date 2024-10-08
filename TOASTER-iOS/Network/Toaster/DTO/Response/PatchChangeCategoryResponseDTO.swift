47ac6687a83641e071b29a4ea91e77dc61667a96
Genesis <dmsgktn1685@naver.com>
2024-10-08 17:10:23 +0900

    [Feat] #217 - 링크 이동 API 관련 DTO 및 Service 구현

---
+++ TOASTER-iOS/Network/Toaster/DTO/Response/PatchChangeCategoryResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  PatchChangeCategoryResponseDTO.swift
+//  TOASTER-iOS
+//
+//  Created by ParkJunHyuk on 10/8/24.
+//
+
+import Foundation
+
+struct PatchChangeCategoryResponseDTO: Decodable {
+    let categoryId: Int
+}
