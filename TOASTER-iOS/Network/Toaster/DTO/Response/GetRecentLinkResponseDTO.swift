94eb91017de13ba5e5b7c4810d45b7e51c2d1cff
mcrkgus <mcrkgus@naver.com>
2024-10-17 18:28:20 +0900

    [Add] #215 - Recent Link Model, DTO

---
+++ TOASTER-iOS/Network/Toaster/DTO/Response/GetRecentLinkResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  GetRecentLinkResponseDTO.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 10/17/24.
+//
+
+import Foundation
---
+++ TOASTER-iOS/Present/Home/Model/RecentLinkModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecentLinkModel.swift
+//  TOASTER-iOS
+//
+//  Created by Gahyun Kim on 10/17/24.
+//
+
+import Foundation
