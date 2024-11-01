778e3d1217905cf2a68b1a261ddbb6d36893fb5b
mcrkgus <mcrkgus@naver.com>
2024-11-02 01:20:27 +0900

    [Chore] #215 - EmptyClipCell Layout 조정

--- TOASTER-iOS/Present/Home/View/Cell/UserClipEmptyCollectionViewCell.swift
+++ TOASTER-iOS/Present/Home/View/Cell/UserClipEmptyCollectionViewCell.swift
@@ -66,13 +66,13 @@ private extension UserClipEmptyCollectionViewCell {
     func setupLayout() {
         addClipImage.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().inset(34)
+            $0.top.equalToSuperview().inset(17)
             $0.size.equalTo(42)
         }
         
         addClipLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(addClipImage.snp.bottom).offset(1)
+            $0.top.equalTo(addClipImage.snp.bottom).offset(5)
         }
     }
     
