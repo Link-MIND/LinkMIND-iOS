20456cdb1dd42426141ee32a353a17009670db92
mcrkgus <mcrkgus@naver.com>
2024-11-12 01:41:06 +0900

    [Refactor] #233 - AddLinkViewModel ViewModelType으로 변경

--- TOASTER-iOS/Present/AddLink/LinkEmbed/ViewModel/AddLinkViewModel.swift
+++ TOASTER-iOS/Present/AddLink/LinkEmbed/ViewModel/AddLinkViewModel.swift
@@ -5,67 +5,69 @@
 //  Created by Gahyun Kim on 9/19/24.
 //
 
+import Combine
 import UIKit
 
-protocol AddLinkViewModelInputs {
-    func embedLinkText(_ text: String)
-}
-
-protocol AddLinkViewModelOutputs {
-    var isClearButtonHidden: Bool { get }
-    var isNextButtonEnabled: Bool { get }
-    var nextButtonBackgroundColor: UIColor { get }
-    var textFieldBorderColor: UIColor { get }
-    var linkEffectivenessMessage: String? { get }
-}
-
-protocol AddLinkViewModelType {
-    var inputs: AddLinkViewModelInputs { get }
-    var outputs: AddLinkViewModelOutputs { get }
-}
-
-final class AddLinkViewModel: AddLinkViewModelType, AddLinkViewModelInputs, AddLinkViewModelOutputs {
+final class AddLinkViewModel: ViewModelType {
     
-    // Input
-    private var embedLink: String = "" {
-        didSet {
-            updateOutputs()
-        }
+    private var cancelBag: CancelBag = CancelBag()
+    
+    struct Input {
+        let embedLinkText: AnyPublisher<String, Never>
     }
     
-    // Output
-    var isClearButtonHidden: Bool
-    var isNextButtonEnabled: Bool
-    var nextButtonBackgroundColor: UIColor
-    var textFieldBorderColor: UIColor
-    var linkEffectivenessMessage: String?
-    
-    init() {
-        self.isClearButtonHidden = true
-        self.isNextButtonEnabled = false
-        self.nextButtonBackgroundColor = .gray200
-        self.textFieldBorderColor = .clear
-        self.linkEffectivenessMessage = nil
+    struct Output {
+        let isClearButtonHidden = PassthroughSubject<Bool, Never>()
+        let isNextButtonEnabled = CurrentValueSubject<Bool, Never>(false)
+        let nextButtonBackgroundColor = CurrentValueSubject<UIColor, Never>(.gray200)
+        let textFieldBorderColor = PassthroughSubject<UIColor, Never>()
+        let linkEffectivenessMessage = PassthroughSubject<String?, Never>()
     }
     
-    func embedLinkText(_ text: String) {
-        embedLink = text
+    func transform(_ input: Input, cancelBag: CancelBag) -> Output {
+        let output = Output()
+        
+        input.embedLinkText
+            .map { $0.isEmpty }
+            .sink { isHidden in
+                output.isClearButtonHidden.send(isHidden)
+            }
+            .store(in: cancelBag)
+        
+        let isValid = input.embedLinkText
+            .map { self.isValidURL($0) }
+            .share()
+            .eraseToAnyPublisher()
+        
+        isValid
+            .combineLatest(input.embedLinkText.map { !$0.isEmpty })
+            .map { $0 && $1 }
+            .sink { isEnabled in
+                print("활성화 유무 : ", isEnabled)
+                output.isNextButtonEnabled.send(isEnabled)
+                output.nextButtonBackgroundColor.send(isEnabled ? .black850 : .gray200)
+            }
+            .store(in: cancelBag)
+        
+        isValid
+            .map { $0 ? .clear : UIColor.toasterError }
+            .sink { color in
+                output.textFieldBorderColor.send(color)
+            }
+            .store(in: cancelBag)
+        
+        input.embedLinkText
+            .map { $0.isEmpty ? "링크를 입력해주세요" : (self.isValidURL($0) ? nil : "유효하지 않은 형식의 링크입니다. " ) }
+            .sink { message in
+                output.linkEffectivenessMessage.send(message)
+            }
+            .store(in: cancelBag)
+        
+        return output
     }
-    
-    var inputs: AddLinkViewModelInputs { return self }
-    var outputs: AddLinkViewModelOutputs { return self }
 }
 
 private extension AddLinkViewModel {
-    func updateOutputs() {
-        let isValid = isValidURL(embedLink)
-        isClearButtonHidden = embedLink.isEmpty
-        isNextButtonEnabled = !embedLink.isEmpty && isValid
-        nextButtonBackgroundColor = isNextButtonEnabled ? .black850 : .gray200
-        textFieldBorderColor = isValid ? .clear : UIColor.toasterError
-        linkEffectivenessMessage = isValid ? nil : (embedLink.isEmpty ? "링크를 입력해주세요" : "유효하지 않은 형식의 링크입니다.")
-    }
-    
     func isValidURL(_ urlString: String) -> Bool {
         if (urlString.prefix(8) == "https://") || (urlString.prefix(7) == "http://") {
             return true
